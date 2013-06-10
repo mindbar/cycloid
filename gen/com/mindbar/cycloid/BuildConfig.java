2db8100e8c0eb7cac85e23497e3f24b9e13e26c5
Andrey Voloshin <voloshin@think.in.ua>
2013-06-10 13:41:19 +0300

    Initial commit

---
+++ gen/com/mindbar/cycloid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mindbar.cycloid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/mindbar/cycloid/CycloidConnectActivity.java
@@ -0,0 +1,15 @@
+package com.mindbar.cycloid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CycloidConnectActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
